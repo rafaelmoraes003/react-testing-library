2684c4d07300e9b49a7880cbebf1dd908e6ae446
rafael <rafaelsm003@gmail.com>
2022-05-09 18:24:03 -0300

    feat: requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import { FavoritePokemons } from '../components';
+import App from '../App';
+
+describe('Testa o componente Favorite Pokemons', () => {
+  test('se é exibida na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noPokemons = screen.getByText('No favorite pokemon found');
+    expect(noPokemons).toBeInTheDocument();
+  });
+
+  test('Teste se são exibidos todos os cards de pokémons favoritados.', () => {
+    const { history } = renderWithRouter(<App />);
+    const moreDetails = screen.getByText('More details');
+    expect(moreDetails).toBeInTheDocument();
+    userEvent.click(moreDetails);
+    const favorite = screen.getByLabelText('Pokémon favoritado?');
+    expect(favorite).toBeInTheDocument();
+    userEvent.click(favorite);
+    const link = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    userEvent.click(link);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+    const imgs = screen.getAllByRole('img');
+    expect(imgs).toHaveLength(2); // imagem do  pokémon e da estrela de favoritado
+  });
+});
